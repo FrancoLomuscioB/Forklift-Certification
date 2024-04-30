c3b91a29b69967ed2f720c5c3c9ac2e8a8be1ba5
Franco <f.lomuscioboccardo@uandresbello.edu>
2024-04-30 18:36:01 -0400

    ForkController inicio

--- Assets/FreeForkLift/Scripts/ForkController.cs
+++ Assets/FreeForkLift/Scripts/ForkController.cs
@@ -3,21 +3,35 @@ using System.Collections;
 
 public class ForkController : MonoBehaviour {
 
-    public Transform fork; 
+    public Transform fork;
     public Transform mast;
     public float speedTranslate; //Platform travel speed
     public Vector3 maxY; //The maximum height of the platform
     public Vector3 minY; //The minimum height of the platform
     public Vector3 maxYmast; //The maximum height of the mast
     public Vector3 minYmast; //The minimum height of the mast
+    public Transform targetRotaion;
+    public Vector3 maxRotX;
+    public Vector3 minRotX;
+    public Vector3 maxX;
+    public Vector3 minX;
+    public float elevValue, sideValue, rotationValue;
+
+
+
+
 
     private bool mastMoveTrue = false; //Activate or deactivate the movement of the mast
+    private bool mastXMoveTrue = false;
 
     // Update is called once per frame
-    void FixedUpdate () {
+    void FixedUpdate()
+    {
 
         Debug.Log(mastMoveTrue);
-        if(fork.transform.localPosition.y >= maxYmast.y && fork.transform.localPosition.y < maxY.y)
+        Vector3 Rotmax = Vector3.RotateTowards(targetRotaion.forward, maxRotX, speedTranslate * Time.deltaTime, 0.0f);
+        Vector3 Rotmin = Vector3.RotateTowards(targetRotaion.forward, minRotX, speedTranslate * Time.deltaTime, 0.0f);
+        if (fork.transform.localPosition.y >= maxYmast.y && fork.transform.localPosition.y < maxY.y)
         {
             mastMoveTrue = true;
         }
@@ -31,16 +45,16 @@ public class ForkController : MonoBehaviour {
         {
             mastMoveTrue = false;
         }
-      
+
         if (Input.GetKey(KeyCode.PageUp))
         {
-           //fork.Translate(Vector3.up * speedTranslate * Time.deltaTime);
+            //fork.Translate(Vector3.up * speedTranslate * Time.deltaTime);
             fork.transform.localPosition = Vector3.MoveTowards(fork.transform.localPosition, maxY, speedTranslate * Time.deltaTime);
-            if(mastMoveTrue)
+            if (mastMoveTrue)
             {
                 mast.transform.localPosition = Vector3.MoveTowards(mast.transform.localPosition, maxYmast, speedTranslate * Time.deltaTime);
             }
-          
+
         }
         if (Input.GetKey(KeyCode.PageDown))
         {
@@ -53,6 +67,56 @@ public class ForkController : MonoBehaviour {
             }
 
         }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            targetRotaion.rotation = Quaternion.LookRotation(Rotmax);
+            if (mastMoveTrue)
+            {
+                mast.transform.localPosition = Vector3.MoveTowards(mast.transform.localPosition, maxYmast, speedTranslate * Time.deltaTime);
+            }
 
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            targetRotaion.rotation = Quaternion.LookRotation(Rotmin);
+            if (mastMoveTrue)
+            {
+                mast.transform.localPosition = Vector3.MoveTowards(mast.transform.localPosition, minYmast, speedTranslate * Time.deltaTime);
+
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            fork.transform.localPosition += Vector3.left * speedTranslate * Time.deltaTime;
+
+
+        }
+
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            // fork.transform.localPosition = Vector3.MoveTowards(fork.transform.localPosition, minX, speedTranslate * Time.deltaTime);
+            fork.transform.localPosition += Vector3.right * speedTranslate * Time.deltaTime;
+
+
+        }
+
+
+    }
+
+    public void GetRotValue(float x)
+    {
+        rotationValue = x;
+    }
+
+    public void GetElevValue(float x)
+    {
+        elevValue = x;
+    }
+
+    public void GetSideValue(float x)
+    {
+        sideValue = x;
     }
 }
