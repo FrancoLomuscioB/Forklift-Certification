1d0aec12a6400913192bdc8a8bb4680ed9c93dc0
Paztero <matiaspino410@gmail.com>
2024-05-07 01:11:45 -0400

    doblajeterminadowuajaja

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,13 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject conoCollider1, conoCollider2, conoCollider3, cono1, cono2, cono3, cono4, yaleObjective1, yaleObjective2, yellowObjective, yaleObjective3, redObjective, greenObjective, yaleObjective4;
 
+    public AudioClip[] test;
+    public List<AudioClip> instruccionesClips;
+    private AudioSource audioActual;
+    public AudioSource radioNoises;
+    public AudioSource radioBeep;
+    public int indiceTest = 0;
+    public int indiceInstrucciones = 0;
     private void Awake()
     {
         if(instance != null)
@@ -25,6 +32,8 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        audioActual = GetComponent<AudioSource>();
+        InstruccionesAudios();
         testCounter = 1;
         conoCounter = 0;
     }
@@ -32,6 +41,65 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       AvanzarInstruccion();
+
+    }
+
+    public void InstruccionesAudios()
+    {
+        if (instruccionesClips.Count == 0)
+        {
+            Debug.Log("asignar una instruccion");
+        }
+        else
+        {
+            audioActual.clip = instruccionesClips[indiceInstrucciones];
+            radioBeep.Play();
+            audioActual.Play();
+            radioNoises.Play();
+        }
+        
+    }
+
+    public void AvanzarInstruccion()
+    {
+        if (!audioActual.isPlaying && indiceInstrucciones == instruccionesClips.Count - 1)
+        {
+            audioActual.Stop();
+            radioNoises.Stop();
+        }
+        else if (!audioActual.isPlaying)
+        {
+            if ((indiceInstrucciones < instruccionesClips.Count - 1))
+            {
+                indiceInstrucciones++;
+                audioActual.clip = instruccionesClips[indiceInstrucciones];
+                audioActual.Play();
+                radioNoises.Play(); 
+            }
+        }
+    }
+    
+
+    public void TestAudios()
+    {
+        if (test.Length == 0)
+        {
+            Debug.Log("asigna un audio");
+        }
+        else
+        {
+            audioActual.clip = test[indiceTest];
+            audioActual.Play();
+        }
+    }
+
+    public void TestActualAudio()
+    {
+        indiceTest = (indiceTest + 1) % test.Length;
+        audioActual.clip = test[indiceTest];
+        audioActual.Play();
+        
 
     }
 
@@ -49,6 +117,7 @@ public class GameManager : MonoBehaviour
                     yaleObjective2.SetActive(true);
                     conoReverse = true;
                     conoCounter = 0;
+                    TestActualAudio();
                 }
                 break;
             case true:
@@ -60,6 +129,7 @@ public class GameManager : MonoBehaviour
                     cono4.SetActive(false);
                     yaleObjective2.SetActive(false);
                     testCounter = 2;
+                    TestActualAudio();
                 }
                 break;
         }
@@ -75,6 +145,7 @@ public class GameManager : MonoBehaviour
             greenObjective.SetActive(true);
             yaleObjective4.SetActive(true);
             testCounter = 3;
+            TestActualAudio();
         }
     }
 
@@ -82,6 +153,7 @@ public class GameManager : MonoBehaviour
     {
         if (greenBox && redBox)
         {
+            TestActualAudio();
             EndGame();
         }
     }
